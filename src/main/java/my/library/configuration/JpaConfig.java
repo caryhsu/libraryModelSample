c2e2c9f4855ccb78ddefec4afd54ad1c964b232e
Tony <Tony@Tony-PC>
2017-07-12 16:25:49 +0800

    modify JpaConfig replace HibernateConfiguration

--- src/main/java/my/library/configuration/JpaConfig.java
+++ src/main/java/my/library/configuration/JpaConfig.java
@@ -3,8 +3,12 @@ package my.library.configuration;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -13,54 +17,57 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-//@Configuration
-//@EnableTransactionManagement
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"my.library.configuration","my.library.dao","my.library.service" })
+@PropertySource(value = {"classpath:application.properties"})
 public class JpaConfig {
 
-//	@Bean
-//	public DataSource dataSource() {
-//		BasicDataSource ds = new BasicDataSource();
-//
-//		ds.setDriverClassName("org.h2.Driver");
-//		ds.setUrl("jdbc:h2:tcp://localhost/~/library");
-//		ds.setUsername("sa");
-//		ds.setPassword("");
-//		ds.setInitialSize(5);
-//		ds.setMaxActive(10);
-//
-//		return ds;
-//	}
-//
-//	@Bean
-//	public JpaVendorAdapter jpaVendorAdapter() {
-//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-//
-//		adapter.setDatabase(Database.H2);
-//		adapter.setShowSql(true);
-//		adapter.setGenerateDdl(false);
-//		adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
-//
-//		return adapter;
-//	}
-//
-//	@Bean
-//	public PlatformTransactionManager jpaTransactionManager() {
-//		JpaTransactionManager tm = new JpaTransactionManager();
-//		tm.setEntityManagerFactory(this.entityManagerFactory().getObject());
-//
-//		return tm;
-//	}
-//
-//	@Bean
-//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-//		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-//		emf.setDataSource(this.dataSource());
-//		emf.setPackagesToScan(
-//				new String[] { 
-//					"my.library.repository", 
-//				});
-//		emf.setJpaVendorAdapter(this.jpaVendorAdapter());
-//
-//		return emf;
-//	}
+    @Autowired
+    private Environment environment;
+	
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource ds = new BasicDataSource();
+		
+        ds.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
+        ds.setUrl(environment.getRequiredProperty("jdbc.url"));
+        ds.setUsername(environment.getRequiredProperty("jdbc.username"));
+        ds.setPassword(environment.getRequiredProperty("jdbc.password"));
+		
+		ds.setInitialSize(5);
+		ds.setMaxActive(10);
+
+		return ds;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+		adapter.setDatabase(Database.H2);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+
+		return adapter;
+	}
+
+	@Bean
+	public PlatformTransactionManager jpaTransactionManager() {
+		JpaTransactionManager tm = new JpaTransactionManager();
+		tm.setEntityManagerFactory(this.entityManagerFactory().getObject());
+
+		return tm;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(this.dataSource());
+		emf.setPackagesToScan("my.library.model");
+		emf.setJpaVendorAdapter(this.jpaVendorAdapter());
+
+		return emf;
+	}
 }
--- src/main/java/my/library/dao/BookRepository.java
+++ src/main/java/my/library/dao/BookRepository.java
@@ -1,8 +1,8 @@
 package my.library.dao;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
 import my.library.model.Book;
@@ -10,32 +10,32 @@ import my.library.model.Book;
 @Repository("bookRepository")
 public class BookRepository {
 
-//	@PersistenceContext
-//	private EntityManager manager;
+	@PersistenceContext
+	private EntityManager manager;
 
-	@Autowired
-	private SessionFactory sessionFactory;
+//	@Autowired
+//	private SessionFactory sessionFactory;
 
 	public Book create(String name, String authors, String ISBN) {
 		Book book = new Book(name);
 		book.setAuthors(authors);
 		book.setISBN(ISBN);
-//		manager.persist(book);
-//		manager.flush();
+		manager.persist(book);
+		manager.flush();
 		return book;
 	}
 
-	protected Session getSession() {
-		return sessionFactory.getCurrentSession();
-	}
-
-	public void persist(Object entity) {
-		getSession().persist(entity);
-	}
-
-	public void delete(Object entity) {
-		getSession().delete(entity);
-	}
+//	protected Session getSession() {
+//		return sessionFactory.getCurrentSession();
+//	}
+//
+//	public void persist(Object entity) {
+//		getSession().persist(entity);
+//	}
+//
+//	public void delete(Object entity) {
+//		getSession().delete(entity);
+//	}
 	
 }
 

00eee4604d76d7d24a999dd0f3c571e0595c808a
Tony <Tony@Tony-PC>
2017-07-12 09:01:43 +0800

    SampleData: add 3 books

--- src/main/java/my/library/model/Record.java
+++ src/main/java/my/library/model/Record.java
@@ -1,5 +1,41 @@
 package my.library.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+
+@Entity
+@Table(name="RECORD")
 public class Record {
 
+	@Id
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="TIME", nullable=false)
+	@Getter private Date time;
+	
+	@ManyToOne()
+	@JoinColumn(name="BOOK_ID", nullable=false)
+	@Getter private Book book;
+	
+	@ManyToOne()
+	@JoinColumn(name="MEMBER_ID", nullable=false)
+	@Getter private Member member;
+	
+	Record() {
+	}
+
+	public Record(Book book, Member member) {
+		this.book = book;
+		this.member = member;
+	}
+	
 }
--- src/test/java/my/library/hibernate/SampleDataBuilder.java
+++ src/test/java/my/library/hibernate/SampleDataBuilder.java
@@ -10,11 +10,40 @@ public class SampleDataBuilder {
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
 		session.beginTransaction();
-		Book book = new Book("億萬商機鳳梨酥！天然手作無添加，居家創業也OK：從炒餡開始，烘烤最安心的彩色金磚【收錄人氣月餅實作】");
-		book.setAuthors("杜佳穎");
-		book.setISBN("9789864082940");
-
-		session.save(book);
+		{
+			Book book = new Book("億萬商機鳳梨酥！天然手作無添加，居家創業也OK：從炒餡開始，烘烤最安心的彩色金磚【收錄人氣月餅實作】");
+			book.setAuthors("杜佳穎");
+			book.setISBN("9789864082940");
+			session.save(book);
+		}
+		{
+			Book book = new Book("名店不敗美味魔法水果塔：塔皮×餡料×鮮果×盤飾醬完美搭配，多層次又不甜膩的好滋味");
+			book.setAuthors("吳佩蓉");
+			book.setISBN("9789863710608");
+			session.save(book);
+		}
+		{
+			Book book = new Book("咖啡聖經");
+			book.setAuthors("Anette Moldvaer");
+			book.setISBN("9789863770916");
+			session.save(book);
+		}
+		{
+			Book book = new Book("咖啡聖經");
+			book.setAuthors("Anette Moldvaer");
+			book.setISBN("9789863770916");
+			session.save(book);
+		}
+		{
+			Book book = new Book("手沖咖啡大全");
+			book.setAuthors("醜小鴨咖啡師訓練中心");
+			book.setISBN("9789863316589");
+			session.save(book);
+		}
+		session.flush();
+		
 		session.getTransaction().commit();
+		
+		HibernateUtil.getSessionFactory().close();
 	}
 }

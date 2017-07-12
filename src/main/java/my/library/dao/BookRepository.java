25c9f1002a913e45373de0c8b686effd654cd5db
Tony <Tony@Tony-PC>
2017-07-12 16:43:03 +0800

    remove BookService and implement BookRepository methods

--- src/main/java/my/library/dao/BookRepository.java
+++ src/main/java/my/library/dao/BookRepository.java
@@ -1,41 +1,64 @@
 package my.library.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
 
 import my.library.model.Book;
+import my.library.model.Member;
 
 @Repository("bookRepository")
+@Transactional
 public class BookRepository {
 
 	@PersistenceContext
-	private EntityManager manager;
-
-//	@Autowired
-//	private SessionFactory sessionFactory;
+	private EntityManager entityManager;
 
 	public Book create(String name, String authors, String ISBN) {
 		Book book = new Book(name);
 		book.setAuthors(authors);
 		book.setISBN(ISBN);
-		manager.persist(book);
-		manager.flush();
+		entityManager.persist(book);
+		entityManager.flush();
+		return book;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Book> findAll() {
+		Query query = entityManager.createQuery("SELECT b FROM Book b");
+	    return (List<Book>) query.getResultList();
+	}
+	
+	public Book get(String id) {
+		return entityManager.find(Book.class, id);
+	}
+
+	public Book update(Book book, String name, String authors, String ISBN) {
+		book = entityManager.merge(book);
+		book.setAuthors(authors);
+		book.setISBN(ISBN);
+		entityManager.merge(book);
+		entityManager.flush();
 		return book;
 	}
 
-//	protected Session getSession() {
-//		return sessionFactory.getCurrentSession();
-//	}
-//
-//	public void persist(Object entity) {
-//		getSession().persist(entity);
-//	}
-//
-//	public void delete(Object entity) {
-//		getSession().delete(entity);
-//	}
-	
+	public void borrowBook(Book book, Member member) {
+		book = entityManager.merge(book);
+		book.borrowBook(member);
+		entityManager.persist(book);
+		entityManager.flush();
+	}
+
+	public void returnBook(Book book) {
+		book = entityManager.merge(book);
+		book.returnBook();
+		entityManager.persist(book);
+		entityManager.flush();
+	}
 }
 
--- src/test/java/AppMain.java
+++ src/test/java/AppMain.java
@@ -4,17 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.AbstractApplicationContext;
 
 import my.library.configuration.AppConfig;
-import my.library.model.Book;
-import my.library.service.BookService;
+import my.library.dao.BookRepository;
 
 public class AppMain {
 
 	public static void main(String args[]) {
 		AbstractApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-		BookService service = (BookService) context.getBean("bookService");
-		service.create("aaa", "bbb", "ccc");
-//		System.out.println();
+		BookRepository bookRepository = (BookRepository) context.getBean("bookRepository");
+		bookRepository.create("aaa", "bbb", "ccc");
+		
 		context.close();
 	}
 }
